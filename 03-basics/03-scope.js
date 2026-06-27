8e3f9539d862ffcd35352af570f81a0f8e215d4a
ayeshaeman347 <emanayesha347@gmail.com>
2026-06-27 16:44:12 +0000

    container config done

---
+++ 03-basics/03-scope.js
@@ -0,0 +1,29 @@
+// let a=10
+// const b=20
+// var c=30
+// if(true){
+//     let a=10
+//     const b=20
+//     var c=30}
+//     //ya jb function ya ifelse ka sath ata hay to ya us ka asciope hoat hay
+//     //if ka bahir use nai ho sakata
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//dakahy var scope ka bahir bhi use ho sakta nuqsan
+if(true){
+    // let a=10
+    // const b=20
+    var c=30}
+    console.log(c);
+    //isis liya var nai use karna chahiya
+    //if ka nadar block scope hay
+    //if ka bahir waha globe scope hay
+    a=10
+    if(true){
+        console.log(a);
+    }
+
+//global scope cod emay or inspect mau alg alag hay
