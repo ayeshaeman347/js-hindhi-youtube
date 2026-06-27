0381774e30f56d81a8b65e417744e58dca9c2b92
ayeshaeman347 <emanayesha347@gmail.com>
2026-06-27 18:00:41 +0000

    container config done

--- 03-basics/03-scope.js
+++ 03-basics/03-scope.js
@@ -27,3 +27,39 @@ if(true){
     }
 
 //global scope cod emay or inspect mau alg alag hay
+
+
+//global scope
+function one(){
+    username="hitesh"
+    console.log(username)
+    function two(){
+        website:"youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    // two()//agr ya call nai ho gi to callone bhi nao chalay gi 
+}
+one()
+//chotay baroo say icecream lay akty baray chootay say nai lay salkty
+//andr wala function bahir walay ka andr hl sakta bahir wala andar walay ka varaiable nai lay 
+//funstion declaration ka liya alg call stack banta hay scope bnta hy isis ko closure kaha jata
+//closure parent child ka varaiables access nai kar saktay but child functiioion ka can access parent functiona
+// yahai kam ifelse may bhi hota hay
+
+//++++++++++++++++++++++interesting++++++++++++++++++
+console.log(addone(7))
+function addone(num){
+    return num+1
+
+}
+// console.log(addone(5))
+
+//expression ki tara variable ki tara hay
+console.log(addtwo(7)) //yaha problem aya gi hoisting how to declare functions
+//error is liya aya ga ku kay varaiable kaandr declare hay ya
+const addtwo=function(num2){
+return num2+2
+}
+// console.log(addtwo(5))
+//hoisting execuation context declaration sat phlay access nai kar kar saktay
